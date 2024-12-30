2432c8f25506e0fd8265ac3260a1a6e7b2dc98f9
Clarice <clariceting2006@gmail.com>
2024-12-30 12:00:44 +0700

    Added MainMenu Function

--- src/App.java
+++ src/App.java
@@ -2,6 +2,19 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner s = new Scanner(System.in);
         
+        do {
+            MainMenu();
+            break;
+        } while (true);
+
+        s.close();
+    }
+
+    public static void MainMenu() {
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
     }
 }
