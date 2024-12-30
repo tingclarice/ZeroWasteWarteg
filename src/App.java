7ce5d05516579dd3876f0c3827f57d0b9eb6c2aa
Angelkyraa17 <awahyudi02@student.ciputra.ac.id>
2024-12-30 12:02:41 +0700

    updated message.json

--- src/App.java
+++ src/App.java
@@ -1,7 +1,18 @@
-import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+       
         
+        switch (jawaban) {
+            case 1:
+                break;
+
+            case 2:
+        
+                break;
+
+            case 3:
+                break;
+        }
     }
 }
